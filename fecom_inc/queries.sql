acc156f117fc6800cd1557e78c0fdcff76c51e02
WhileCrocodile <98533784+WhileCrocodile@users.noreply.github.com>
2025-04-03 02:29:06 -0400

    Update progress

--- fecom_inc/queries.sql
+++ fecom_inc/queries.sql
@@ -41,7 +41,31 @@ CREATE TABLE fecom_inc_order_reviews_no_emojis(
 	Review_Comment_Message_En VARCHAR(10000),
 	Review_Creation_Date VARCHAR(50),
 	Review_Answer_Timestamp VARCHAR(50));
+    
+CREATE TABLE fecom_inc_orders(
+	Order_ID CHAR(32),
+    Customer_Trx_ID CHAR(32),
+    Order_Status VARCHAR(100),
+    Order_Purchase_Timestamp VARCHAR(100),
+    Order_Approved_At VARCHAR(100),
+    Order_Delivered_Carrier_Date VARCHAR(100),
+    Order_Delivered_Customer_Date VARCHAR(100),
+    Order_Estimated_Delivery_Date VARCHAR(100));
+    
+CREATE TABLE fecom_inc_products(
+	Product_ID CHAR(32),
+	Product_Category_Name VARCHAR(100),
+    Product_Weight_Gr MEDIUMINT,
+    Product_Length_Cm SMALLINT,
+    Product_Height_Cm SMALLINT,
+    Product_Width_Cm SMALLINT);
+    
+CREATE TABLE fecom_inc_sellers_list(
+	Seller_ID CHAR(32),
+    Seller_Name VARCHAR(100),
+    Seller_Postal_Code VARCHAR(100),
+    Seller_City VARCHAR(100),
+    Country_Code VARCHAR(100),
+    Seller_Country VARCHAR(100));
 
-DROP TABLE fecom_inc_order_reviews_no_emojis;
-SHOW TABLES;
-SELECT * FROM fecom_inc_geolocations;
+SELECT * FROM fecom_inc_sellers_list;
