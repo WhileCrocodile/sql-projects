18812f9184e46eebdbd488a7cbe425de84b289d7
WhileCrocodile <98533784+WhileCrocodile@users.noreply.github.com>
2025-04-08 17:41:42 -0400

    Updated README and generation script

--- generate_README.py
+++ generate_README.py
@@ -1,16 +1,14 @@
 # Regenerates the README.md based on the data from project_info.csv
 import csv
 
-readme_intro = '''This repository stores the results of my personal projects done in SQL.'''
-
 # Converts a list of entries into a markdown table row
-def array_to_tablerow(array):
+def list_to_mdrow(rowlist):
     row = "| "
-    for entry in array:
+    for entry in rowlist:
         row += entry + " | "
     return row
 
-if __name__ == "__main__":
+def csv_to_mdtable(path_to_csv):
     with open("projects_info.csv", newline='') as csvfile:
         csvreader = csv.reader(csvfile)
         csv_rows = []
@@ -20,11 +18,20 @@ if __name__ == "__main__":
     csv_rows.insert(1, ["---" for i in csv_rows[0]]) # Insert something to represent the header separator line
     markdown_table = ""
     for one_row in csv_rows:
-        markdown_table += array_to_tablerow(one_row) + "\n"
-    readme_intro += "\n\n" + markdown_table
+        markdown_table += list_to_mdrow(one_row) + "\n"
+    return markdown_table
 
-    with open("README.md", "w") as readmefile:
-        readmefile.write(readme_intro)
+
+if __name__ == "__main__":
+    path_to_csv = 'projects_info.csv'
+    with open("readme_template.txt", newline='') as readme_template:
+        readme_text = readme_template.read()
+    
+    markdown_table = csv_to_mdtable(path_to_csv)
+    readme_text = readme_text.replace(f"({path_to_csv})", markdown_table)
+    print(readme_text)
+    with open("README.md", "w", newline='') as readme_file:
+        readme_file.write(readme_text)
 
 
         
